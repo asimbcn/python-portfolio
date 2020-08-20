cd5315cd36f88a31c7e1dcd9d039c866f943b735
Asim Raj Pandey <asimbcn@gmail.com>
2020-08-20 17:22:54 +0545

    error page and redirect

--- Portfolio/User/views.py
+++ Portfolio/User/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User, UserProfile, Work, Education, Project
 
 # Create your views here.
 def index(request):
-    user = User.objects.get(active = True)
-    print(user)
-    profile = UserProfile.objects.get(vuser = user)
-    work = Work.objects.filter(user = user)
-    education = Education.objects.filter(user = user)
-    project = Project.objects.filter(user = user)
-    print(project,work,education,project)
-    return render(request,'index.html')
+    try:
+        user = User.objects.get(active = True)
+        profile = UserProfile.objects.get(vuser = user)
+        work = Work.objects.filter(user = user)
+        education = Education.objects.filter(user = user)
+        project = Project.objects.filter(user = user)
+        return render(request,'index.html',{'profile':profile,'work':work,'education':education,'project':project})
+    except:
+        return redirect('error404')
+            
+def error(request):
+    return render(request,'error.html')   
