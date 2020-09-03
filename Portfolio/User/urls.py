8c32121490c891c9fa50b127e8b9dd2336040663
Asim Raj Pandey <asimbcn@gmail.com>
2020-09-03 17:44:48 +0545

    Link Changes

--- Portfolio/User/urls.py
+++ Portfolio/User/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('error/',views.error,name='error404'),
-    path('contact/',views.contact,name='contact'),
+    path("", views.index, name="home"),
+    path("error/", views.error, name="error404"),
+    path("contact/", views.contact, name="contact"),
 ]
--- Portfolio/User/views.py
+++ Portfolio/User/views.py
@@ -1,41 +1,57 @@
 from django.shortcuts import render, redirect
 from .models import User, UserProfile, Work, Education, Project, Contact
 
+
 # Create your views here.
 def index(request):
     try:
-        user = User.objects.get(active = True)
-        profile = UserProfile.objects.get(vuser = user)
-        work = Work.objects.filter(user = user)
-        education = Education.objects.filter(user = user)
-        project = Project.objects.filter(user = user)
-        return render(request,'index.html',{'profile':profile,'work':work,'education':education,'project':project})
+        user = User.objects.get(active=True)
+        profile = UserProfile.objects.get(vuser=user)
+        work = Work.objects.filter(user=user)
+        education = Education.objects.filter(user=user)
+        project = Project.objects.filter(user=user)
+        return render(
+            request,
+            "index.html",
+            {
+                "profile": profile,
+                "work": work,
+                "education": education,
+                "project": project,
+            },
+        )
     except:
-        return redirect('error404')
-        
-            
+        return redirect("error404")
+
+
 def error(request):
-    return render(request,'error.html') 
+    return render(request, "error.html")
+
 
 def contact(request):
-    if request.method == 'POST':
-        if request.POST['name'] != '' and request.POST['email'] != '' and request.POST['subject'] != '' and request.POST['message'] != '' :
+    if request.method == "POST":
+        if (
+            request.POST["name"] != ""
+            and request.POST["email"] != ""
+            and request.POST["subject"] != ""
+            and request.POST["message"] != ""
+        ):
             contact = Contact()
-            contact.name = request.POST['name']
-            contact.email = request.POST['email']
-            contact.subject = request.POST['subject']
-            contact.description = request.POST['message']
+            contact.name = request.POST["name"]
+            contact.email = request.POST["email"]
+            contact.subject = request.POST["subject"]
+            contact.description = request.POST["message"]
             try:
-                user = User.objects.get(username = request.POST['user'])
+                user = User.objects.get(username=request.POST["user"])
                 contact.user = user
             except:
-                return render(request,'error.html')    
+                return render(request, "error.html")
             try:
                 contact.save()
-                return redirect('home')
+                return redirect("home")
             except:
-                return render(request,'error.html')      
+                return render(request, "error.html")
         else:
-            return render(request,'error.html')
+            return render(request, "error.html")
     else:
-        return redirect('home')         
+        return redirect("home")
