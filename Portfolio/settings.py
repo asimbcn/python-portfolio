7e10135d52180473ed8b8cdcd4126fd9f6720566
Asim Raj Pandey <53789278+asimbcn@users.noreply.github.com>
2020-09-30 22:28:08 +0545

    static changes for heroku

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '2#w$y5%5ywq(+bgfk&x8-fx=$nx!o44vy+jjqek2&f*#7x3#f-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ["asimbcn-portfolio.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["asimbcn-portfolio.herokuapp.com", "127.0.0.1"]
 
 # Application definition
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
