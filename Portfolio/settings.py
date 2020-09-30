84bf2840ce40ec543cf7b9c08249017265d6b70d
Asim Raj Pandey <asimbcn@gmail.com>
2020-09-30 21:53:16 +0545

    Heroku media changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 AUTH_USER_MODEL = 'User.User'
